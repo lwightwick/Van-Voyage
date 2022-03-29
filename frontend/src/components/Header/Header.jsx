a41125bf94d814e4b6656c44fdab0796a39d2742
lwightwick <wightwick.lucy21@gmail.com>
2022-03-28 21:40:48 -0700

    styling header

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,43 +1,56 @@
 import "./Header.scss";
-import { Link } from "react-router-dom";
-import Logo from "../../assets/Icons/logo.svg";
+import Nav from "../Nav/Nav";
 
 function Header() {
   return (
-    <header className="header">
-      <section className="header__container">
-        <div>
-          <Link to="/home" key="home-link">
-            <img className="logo" src={Logo} alt="logo" />
-          </Link>
-        </div>
-
-        <Link className="header__link" to="/van-details" key="van-details-link">
-          Van Details
-        </Link>
-
-        <Link className="header__link" to="/about" key="about-link">
-          about
-        </Link>
-
-        <Link className="header__link" to="/booking" key="booking-link">
-          book my Trip
-        </Link>
-
-        <Link className="header__link" to="/contact" key="contact-link">
-          contact
-        </Link>
-
-        <Link
-          className="header__link"
-          to="/places-to-go"
-          key="places-to-go-link"
-        >
-          Places to Go
-        </Link>
-      </section>
-    </header>
+    <div>
+      <Nav />
+    </div>
   );
 }
+
+// function Header() {
+//   return (
+//     <header className="header">
+//       <section className="header__container">
+//         <div className="header__logo">
+//           <Link to="/home" key="home-link">
+//             <img className="logo" src={Logo} alt="logo" />
+//           </Link>
+//         </div>
+
+//         <div>
+//           <span />
+//           <span />
+//           <span />
+//         </div>
+
+//         <Link className="header__link" to="/van-details" key="van-details-link">
+//           Van Details
+//         </Link>
+
+//         <Link className="header__link" to="/about" key="about-link">
+//           about
+//         </Link>
+
+//         <Link className="header__link" to="/booking" key="booking-link">
+//           book my Trip
+//         </Link>
+
+//         <Link className="header__link" to="/contact" key="contact-link">
+//           contact
+//         </Link>
+
+//         <Link
+//           className="header__link"
+//           to="/places-to-go"
+//           key="places-to-go-link"
+//         >
+//           Places to Go
+//         </Link>
+//       </section>
+//     </header>
+//   );
+// }
 
 export default Header;
