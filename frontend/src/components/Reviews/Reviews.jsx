ed00ef03a59cb4038bd44809c91e9edc022f74e2
lwightwick <wightwick.lucy21@gmail.com>
2022-04-02 16:16:41 -0700

    reviews autoplay slider renderd and styled all screens

--- frontend/src/components/Reviews/Reviews.jsx
+++ frontend/src/components/Reviews/Reviews.jsx
@@ -1,24 +1,37 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ReviewsData from "../../data/reviews.json";
 import "./Reviews.scss";
 
 function Reviews() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+  };
+
   return (
     <div className="reviews">
-      <h2>reviews</h2>
-      <div className="reviews__container">
-        <div className="reviews__card">
+      <h2 className="reviews__title">reviews</h2>
+      <div className="slider-wrapper">
+        <Slider {...settings}>
           {ReviewsData.map((item, index) => (
-            <div className="reviews__" key={index}>
-              <p className="reviews__text">{item.review}</p>
+            <div className="slick-slide" key={index}>
+              <p className="slick-slide__text">{item.review}</p>
               <img
-                className="reviews__image"
+                className="slick-slide__image"
                 src={item.image}
                 alt={item.title}
               />
             </div>
           ))}
-        </div>
+        </Slider>
       </div>
     </div>
   );
