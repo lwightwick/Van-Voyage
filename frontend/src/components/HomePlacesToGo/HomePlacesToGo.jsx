24a92e6a1396d1ea4be242b393bea9d09e90f8ad
lwightwick <wightwick.lucy21@gmail.com>
2022-04-03 20:37:10 -0700

    updates places to go

--- frontend/src/components/HomePlacesToGo/HomePlacesToGo.jsx
+++ frontend/src/components/HomePlacesToGo/HomePlacesToGo.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../Button/Button";
-import BannerImage from "../../assets/Images/van-mountains.png";
+import SunshineCost from "../../assets/Images/sunshine-coast.jpg";
+import Lillooet from "../../assets/Images/lillooet.jpg";
+import Pender from "../../assets/Images/pender.jpg";
 import "./HomePlacesToGo.scss";
 
 function HomePlacesToGo() {
@@ -9,27 +11,40 @@ function HomePlacesToGo() {
     <div>
       <section className="places_to_go">
         <h2>Places To Go</h2>
-        <div className="places_to_go__list-container">
-          <ul className="places_to_go__list">
-            <li>
-              <img src={Image} alt="" />
-              <Link to="/places-to-go">
-                <Button text={"Find Out More"} />
-              </Link>
-            </li>
-            <li>
-              <img src={Image} alt="" />
-              <Link to="/places-to-go">
-                <Button text={"Find Out More"} />
-              </Link>
-            </li>
-            <li>
-              <img src={Image} alt="" />
-              <Link to="/places-to-go">
-                <Button text={"Find Out More"} />
-              </Link>
-            </li>
-          </ul>
+        <div className="places_to_go__container">
+          <div className="places_to_go__card">
+            <h2 className="places_to_go__subtitle">Sunshine Coast</h2>
+            <img
+              className="places_to_go__image"
+              src={SunshineCost}
+              alt="sunshine coast"
+            />
+            <Link to="/places-to-go">
+              <Button text={"Find Out More"} />
+            </Link>
+          </div>
+          <div className="places_to_go__card">
+            <h2 className="places_to_go__subtitle">Pender Island</h2>
+            <img
+              className="places_to_go__image"
+              src={Pender}
+              alt="pender island"
+            />
+            <Link to="/places-to-go">
+              <Button text={"Find Out More"} />
+            </Link>
+          </div>
+          <div className="places_to_go__card">
+            <h2 className="places_to_go__subtitle">Lillooet</h2>
+            <img
+              className="places_to_go__image"
+              src={Lillooet}
+              alt="Lillooet"
+            />
+            <Link to="/places-to-go">
+              <Button text={"Find Out More"} />
+            </Link>
+          </div>
         </div>
       </section>
     </div>
