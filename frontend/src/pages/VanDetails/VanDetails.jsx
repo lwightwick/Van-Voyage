83c5b28c83d922a8af604ee796c8daf1012a139d
lwightwick <wightwick.lucy21@gmail.com>
2022-04-06 19:02:33 -0700

    last styling edits

--- frontend/src/pages/VanDetails/VanDetails.jsx
+++ frontend/src/pages/VanDetails/VanDetails.jsx
@@ -7,7 +7,12 @@ import VirtualTour from "../../components/VirtualTour/VirtualTour";
 function VanDetails() {
   return (
     <main className="van-page">
-      <h1 className="van-page__title">about brandi</h1>
+      <div className="van-page__title-container">
+        <h1 className="van-page__title">about brandi vanlisle</h1>
+        <p className="van-page__description">
+          (Named after Alice's favourite singer-songwriter, Brandi Carlisle)
+        </p>
+      </div>
       <VanImageSlider slides={VanImagesData} />
       <VirtualTour />
       <BehindScenesImageGallery />
